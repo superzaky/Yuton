b7f53bba0dd0d8844c60ea78fffbb9b7a3932fbe
kailashyogeshwar85 <kailash.yogeshwar@raweng.com>
2015-11-15 22:48:35 +0530

    Resolved the issue

--- packages/custom/myTheme/public/controllers/myTheme.js
+++ packages/custom/myTheme/public/controllers/myTheme.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.myTheme').controller('MyThemeController', ['$scope', 'Global', 'MyTheme',
+angular.module('mean.myTheme').controller('ThemeController', ['$scope', 'Global', 'MyTheme',
   function($scope, Global, MyTheme) {
     $scope.global = Global;
     $scope.package = {
