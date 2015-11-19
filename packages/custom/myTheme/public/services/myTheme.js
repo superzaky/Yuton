457719657474ea4b8d38444cb4b0d7c0f01f646b
Zaky Huraibi <superkytoz@gmail.com>
2015-11-19 13:02:58 +0100

    User gets now logged in after he had made an account

--- packages/custom/myTheme/public/services/myTheme.js
+++ packages/custom/myTheme/public/services/myTheme.js
@@ -3,7 +3,7 @@
 var myApp = angular.module('mean.myTheme');
 
 //factory style, more involved but more sophisticated
-myApp.factory('MyTheme', function($http, $location) {
+myApp.factory('MyTheme', function($http, $location, MeanUser) {
     return {
         register: function(user) {
             return $http.post('/api/myTheme/register', { 
@@ -13,14 +13,7 @@ myApp.factory('MyTheme', function($http, $location) {
                 username: user.username,
                 name: user.name
             }).then(function successCallback(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-                $http.post('/api/login', {
-                    email: user.email,
-                    password: user.password
-                }).then(function (response) {
-                    $location.path( "/" );
-                }); 
+              MeanUser.login(user);
 
             }, function errorCallback(response) {
             // called asynchronously if an error occurs
